d5e8becea9ab60bce94b034b9e7a28537186717b
kprakash18 <iamkprakash18@gmail.com>
2026-08-15 15:15:51 +0530

    detect text selection

--- content/content.js
+++ content/content.js
@@ -1,1 +1,11 @@
-console.log("auto copy content is loaded") ;
+// selecting text 
+document.addEventListener('selectionchange', () => {
+  const selection = document.getSelection();
+  const selectedText = selection ? selection.toString() : '';
+
+  if (selectedText.trim()) {
+    console.log('User selected text:', selectedText);
+  } else {
+    console.log('Selection cleared');
+  }
+});
